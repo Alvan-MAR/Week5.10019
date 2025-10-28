b7ce42b13258a63a54e1a34f3386337fe59ecf74
Alvan-MAR <alvanrizky2009@gmail.com>
2025-10-28 13:23:03 +0700

    first commit

---
+++ PreTask1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PreTask1 {
+    public static void main(String[] args) {
+        double alas, tinggi, sisi2, sisi3, keliling, luas;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Tolong masukkan nilai alas segitiga: ");
+        alas = input.nextDouble();
+
+        System.out.println("Tolong masukkan nilai tinggi segitiga: ");
+        tinggi = input.nextDouble();
+
+        System.out.println("Tolong masukkan nilai sisi 2 segitiga: ");
+        sisi2 = input.nextDouble();
+
+        System.out.println("Tolong masukkan nilai sisi 3 segitiga: ");
+        sisi3 = input.nextDouble();
+        input.close();
+
+
+        keliling = alas + sisi2 + sisi3;
+        luas = 0.5 * alas * tinggi;
+
+        System.out.println("Keliling segitiga adalah: " + keliling);
+        System.out.println("Luas segitiga adalah: " + luas);
+
+
+
+
+
+    }
+    
+}
