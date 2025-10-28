08c33c8d7ca5bc5c1f7e31426e9ccce5fd793e9c
Alvan-MAR <alvanrizky2009@gmail.com>
2025-10-28 13:31:22 +0700

    Revision2

--- PreTask1.java
+++ PreTask1.java
@@ -5,16 +5,16 @@ public class PreTask1 {
         double alas, tinggi, sisi2, sisi3, keliling, luas;
         Scanner input = new Scanner(System.in);
         
-        System.out.print("Tolong masukkan nilai alas segitiga: ");
+        System.out.print("Tolong masukkan nilai alas segitiga (cm): ");
         alas = input.nextDouble();
 
-        System.out.print("Tolong masukkan nilai tinggi segitiga: ");
+        System.out.print("Tolong masukkan nilai tinggi segitiga (cm): ");
         tinggi = input.nextDouble();
 
-        System.out.print("Tolong masukkan nilai sisi 2 segitiga: ");
+        System.out.print("Tolong masukkan nilai sisi 2 segitiga (cm): ");
         sisi2 = input.nextDouble();
 
-        System.out.print("Tolong masukkan nilai sisi 3 segitiga: ");
+        System.out.print("Tolong masukkan nilai sisi 3 segitiga (cm): ");
         sisi3 = input.nextDouble();
         input.close();
 
@@ -22,8 +22,8 @@ public class PreTask1 {
         keliling = alas + sisi2 + sisi3;
         luas = 0.5 * alas * tinggi;
 
-        System.out.println("Keliling segitiga adalah: " + keliling);
-        System.out.println("Luas segitiga adalah: " + luas);
+        System.out.println("Keliling segitiga adalah: " + keliling + " cm");
+        System.out.println("Luas segitiga adalah: " + luas + " cm^2");
 
 
 
